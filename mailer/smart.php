babe1e9e6a2920276f1e244c70e998a296d2ab39
gulnaz.web <73237809+gulnaz-web@users.noreply.github.com>
2021-02-03 13:02:36 +0300

    Add files via upload

--- mailer/smart.php
+++ mailer/smart.php
@@ -1,11 +1,17 @@
 <?php 
 
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-$text = $_POST['text'];
-$checkbox = $_POST['checkbox'];
+// var_dump($_POST);
+// var_dump($_GET);
 
+$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+$phone = filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_SPECIAL_CHARS);
+$text = filter_input(INPUT_POST, 'text', FILTER_SANITIZE_SPECIAL_CHARS);
+$checkbox = filter_input(INPUT_POST, 'checkbox', FILTER_SANITIZE_SPECIAL_CHARS);
 
+// $name = $_POST['name'];
+// $phone = $_POST['phone'];
+// $text = $_POST['text'];
+// $checkbox = $_POST['checkbox'];
 
 require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
@@ -14,14 +20,14 @@ $mail->CharSet = 'utf-8';
 // $mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.com';  // Specify main and backup SMTP servers
+$mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication - входим на почту
 $mail->Username = 'bankrotstvo91@bk.ru';                 // Наш логин
 $mail->Password = 'aEU2-eHgueH1';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
 
-$mail->setFrom('bankrotstvo91@bk.ru', 'Saceza');   // От кого письмо 
+$mail->setFrom('bankrotstvo91@bk.ru');   // От кого письмо 
 $mail->addAddress('netbankrotstvu@mail.ru');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
@@ -37,11 +43,14 @@ $mail->Body    = '
 	Имя: ' . $name . ' <br>
 	Номер телефона: ' . $phone . '<br>
 	Текст: ' . $text . '';
-
-if(!$mail->send()) {
-    return false;
-} else {
-    return true;
-}
-
+$mail->AltBody = '';
+	
+	if(!$mail->send()) {
+		$message = 'Ошибка';
+	} else {
+		$message = 'Ваша заявка отправлена';
+	}
+	$response = ['message' => $message];
+	header('Content-type: application/json');
+	echo json_encode($response);
 ?>
