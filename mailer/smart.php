e0d055308f7f30c3eac9c883f699ce7ae92bee08
gulnaz.web <73237809+gulnaz-web@users.noreply.github.com>
2021-02-08 15:37:06 +0300

    Update smart.php

--- mailer/smart.php
+++ mailer/smart.php
@@ -22,13 +22,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication - входим на почту
-$mail->Username = 'bankrotstvo91@bk.ru';                 // Наш логин
-$mail->Password = 'aEU2-eHgueH1';                           // Наш пароль от ящика
+$mail->Username = '';                 // Наш логин
+$mail->Password = '';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
 
-$mail->setFrom('bankrotstvo91@bk.ru');   // От кого письмо 
-$mail->addAddress('netbankrotstvu@mail.ru');     // Add a recipient
+$mail->setFrom('');   // От кого письмо 
+$mail->addAddress('');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
